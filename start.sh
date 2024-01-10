e9617da655b5eb0c5e77df0ccbef72ae15beedba
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-01-10 12:30:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jayanth14072003/MJ-Filter-Bot.git /MJ
+  git https://github.com/Girish019/Alexa-Filter-Bot-V2/tree/MJ
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MJ_FILTER_BOT 
+  git clone $UPSTREAM_REPO /Alexa_FILTER_BOT 
 fi
-cd /MJ_FILTER_BOT 
+cd /Alexa_FILTER_BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
